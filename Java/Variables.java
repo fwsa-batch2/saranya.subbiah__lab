72293896e172304399ff6286b3bee44c161596e8
Saran-20 <Saranyaselv2820@gmail.com>
2022-03-30 15:30:30 +0530

    update

---
+++ Java/Variables.java
@@ -0,0 +1,12 @@
+public class main {
+     public static void main(String[] args) {
+          int myNum = 10;
+          float myFloatNum = 5.99f;    
+          char myLetter = 'S';         
+          boolean myBool = false;       
+          String myText = "Saran";
+          System.out.println(myNum  + myLetter + myText);
+          System.out.println(myBool);
+          System.out.println(myFloatNum);
+     }
+}
