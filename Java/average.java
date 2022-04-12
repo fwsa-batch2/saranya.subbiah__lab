fd453a3eaef64eab1769786d6375c4ce9abdd2ce
Saran-20 <Saranyaselv2820@gmail.com>
2022-04-12 12:44:27 +0530

    add

---
+++ Java/average.java
@@ -0,0 +1,13 @@
+public class average{
+    public static void main (String[] args){
+        int n1,n2, n3, n4, n5, total;
+        n1 = 89;
+        n2 = 99;
+        n3 = 98;
+        n4 = 95;
+        n5 = 96;
+        total = n1+n2+n3+n4+n5;
+        System.out.println("Average of the given values=" +total / 5);
+    }
+      
+}
