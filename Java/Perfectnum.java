a97eb1208742dce9d1ad2cc8d8507cab1c741ac0
Saran-20 <Saranyaselv2820@gmail.com>
2022-04-19 15:34:31 +0530

    add

--- Java/Perfectnum.java
+++ Java/Perfectnum.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Perfectnum {
-    
+    public static void main(String[]args) {
+        int i, num, Sum = 0;
+    System.out.println("Enter any Number");
+    Scanner sc = new Scanner(System.in);
+    num = sc.nextInt();
+
+    for(i=0; i<num; i++) {
+       if (num%i==0) {
+           Sum = Sum + i;
+       }
+    }
+
+    if(Sum==num) {
+        System.out.println(num + "is a Perfect Number");
+    }
+    else {
+        System.out.println(num + "is not a Perfect Number");
+    }
+    }
 }
--- Java/Print.java
+++ Java/Print.java
@@ -1,5 +1,5 @@
-public class main {
-    public static void main(String[] args){
+public class Print {
+    public static void main (String[] args){
         System.out.print("Hello world");
     }
 } 
