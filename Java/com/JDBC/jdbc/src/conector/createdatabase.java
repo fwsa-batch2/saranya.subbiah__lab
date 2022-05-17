974d9fb6ca9e6f8b7c54cba438d7ec03395b386f
Saran-20 <Saranyaselv2820@gmail.com>
2022-05-17 11:50:32 +0530

    add

---
+++ Java/com/JDBC/jdbc/src/conector/createdatabase.java
@@ -0,0 +1,21 @@
+package conector;
+import java.sql.*;
+public class jdbc {
+		public static void main(String[] args) {
+			String url="jdbc:mysql://localhost:3306";
+			String user = "Saran";
+			String password = "Saranya";
+			try {
+//				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection connection=DriverManager.getConnection(url,user,password);
+				String sql="Create database lo" ;
+				Statement fresh= connection.createStatement();
+				fresh.execute(sql);
+				System.out.println("Database create agiduchu da venna");
+			}catch(Exception e){
+				System.out.println("illa veliya po da");
+				e.printStackTrace();
+			}
+		}
+	}
+
