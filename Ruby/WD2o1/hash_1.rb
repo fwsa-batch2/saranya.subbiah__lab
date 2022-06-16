2cd47464ff3b75dc092a721cfa6fa6a314ee9a9e
Saran-20 <Saranyaselv2820@gmail.com>
2022-06-16 17:06:35 +0530

    add

--- Ruby/WD2o1/hash_1.rb
+++ Ruby/WD2o1/hash_1.rb
@@ -1,2 +1,13 @@
 books = ["Design as Art", "Anathem", "Shogun"]
 authors = ["Bruno Munari", "Neal Stephenson", "James Clavell"]
+num = {}
+for val in 0...books.length do
+    for a in 0...authors.length do
+        if val == a
+            b = authors[a].split.first.downcase.to_sym
+            c = books[val]
+            num.store(b,c)
+        end
+    end
+end
+puts num
